c3e234c3c206e4753b0e4368f2dcbf37829d6bc0
islaytitans <jonthan.m.robbins@gmail.com>
2015-06-22 13:40:16 +0100

    Added initial function to retrieve renderings and their datasource and pass it back

--- SitecoreContentEditorTabs/Controllers/ComponentController.cs
+++ SitecoreContentEditorTabs/Controllers/ComponentController.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Layouts;
 using Sitecore.Services.Core;
 using Sitecore.Services.Infrastructure.Sitecore.Services;
 using SitecoreContentEditorTabs.Models;
@@ -22,9 +27,30 @@ namespace SitecoreContentEditorTabs.Controllers
 
         public List<Component> GetComponents(string itemId)
         {
-            var test = new List<Component>();
+            Assert.IsNotNullOrEmpty(itemId, "itemId");
 
-            test.Add(new Component() {Id = "3", ComponentName = "News", DatasourceId = new Guid(), DatasourceName = "The article"});
+            var database = Sitecore.Data.Database.GetDatabase("master");
+            var item = database.GetItem(itemId);
+            if (item == null || item.Fields["__renderings"] == null || string.IsNullOrEmpty(item.Fields["__renderings"].Value))
+                return null;
+
+            var layoutField = (Sitecore.Data.Fields.LayoutField) item.Fields["__renderings"];
+
+            var references = layoutField.GetReferences(new DeviceItem(null));
+
+            var test = (from reference in references
+                let renderingItem = reference.RenderingItem
+                let datasource = database.GetItem(renderingItem.DataSource)
+                select new Component()
+                {
+                    Id = "1",
+                    ComponentName = reference.RenderingItem.Name,
+                    DatasourceId = datasource.ID.ToGuid(),
+                    DatasourceLink = datasource.Paths.FullPath,
+                    DatasourceName = datasource.Name,
+                }).ToList();
+
+            test.Add(new Component() { Id = "3", ComponentName = "News", DatasourceId = new Guid(), DatasourceName = "The article" });
 
             return test;
         }
